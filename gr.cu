f3d7e572dbc1a77aafc62594e717a12f773b19f0
Calvin Huang <clhuang@eneron.us>
2013-07-29 16:01:13 -0700

    Added additional functions--may need to rename repo

--- gr.cu
+++ gr.cu
@@ -1,13 +1,17 @@
-__constant__ int numFreqs = %d;
-__constant__ int numPoints = %d;
+__constant__ int numFreqs;
+__constant__ int numPoints;
 
 /**
   Output is numPoints * 3 array: height, center, standard deviation
   Input frequencies in nm to avoid underflow
 */
 __global__ void gaussReg(float *input, float *output, float *frequencies, float cutoffPortion){
-    input = input + (threadIdx.x + blockDim.x * blockIdx.x) * numFreqs;
-    output = output + (threadIdx.x + blockDim.x * blockIdx.x) * 3;
+    int idx = threadIdx.x + blockDim.x * blockIdx.x;
+    
+    if (idx >= numPoints) return;
+
+    input += idx * numFreqs;
+    output += idx * 3;
 
     float threshold = input[0];
     float maxFreq = frequencies[0];
@@ -77,4 +81,49 @@ __global__ void gaussReg(float *input, float *output, float *frequencies, float 
     output[1] = -b / scaleFactor / (2 * a) + maxFreq;
     output[2] = sqrtf(-1 / (2 * a)) / scaleFactor;
     
+}
+
+__global__ void findPeaks(float *input, float *output, float *frequencies) {
+    int idx = threadIdx.x + blockDim.x * blockIdx.x;
+    
+    if (idx >= numPoints) return;
+
+    input = input + idx * numFreqs;
+    output = output + idx;
+
+    float maxIntensity = input[0];
+    float maxFrequency = 0;
+
+    for (int i = 0; i < numFreqs; i++) {
+        if (input[i] > maxIntensity){
+            maxIntensity = input[i];
+            maxFrequency = frequencies[i];
+        }
+    }
+
+    output[0] = maxFrequency;
+}
+
+__global__ void integrate(float *input, float *output, float *target, float *frequencies, bool left) {
+    int idx = threadIdx.x + blockDim.x * blockIdx.x;
+    
+    if (idx >= numPoints) return;
+
+    input = input + idx * numFreqs;
+    output = output + idx;
+
+    float targetFrequency = frequencies[idx];
+    float integral = 0;
+    
+    if (left) {
+        for (int i = 1; frequency[i] < targetFrequency; i++) {
+            integral += (output[i] + output[i-1]) * (frequency[i] - frequency[i-1]) / 2;
+        }
+    } else { //right
+        for (int i = numFreqs - 1; frequency[i] > targetFrequency; i++) {
+            integral += (output[i] + output[i+1]) * (frequency[i+1] - frequency[i]) / 2;
+        }
+    }
+
+    output[0] = integral;
 }
--- gr.py
+++ gr.py
@@ -10,13 +10,16 @@ file = open(os.path.dirname(__file__) + '/gr.cu')
 cudaCode = file.read()
 file.close()
 
+mod = SourceModule(cudaCode)
+
 def gaussRegression(data, frequencies):
     '''
     data is a ndata*nfrequency array
     frequencies is a nfrequency array
     returns an ndata*3 array with height, center frequency, and standard deviation
     '''
-    ndata, nfrequencies = data.shape
+    data = data.T #change to nfrequency * ndata
+    nfrequencies, ndata = data.shape
     out = np.empty((ndata, 3), 'float32')
     mod = SourceModule(cudaCode %(nfrequencies, ndata))
 
@@ -24,7 +27,31 @@ def gaussRegression(data, frequencies):
     gridSize = (ndata + blockSize - 1) / blockSize
     greg = mod.get_function('gaussReg')
 
-    greg(cuda.In(data.T.astype('float32')), cuda.Out(out), cuda.In(frequencies.astype('float32')), np.float32(0.5),
+    cuda.memcpy_htod(mod.get_global('numFreqs')[0], np.int32(nfrequencies))
+    cuda.memcpy_htod(mod.get_global('numPoints')[0], np.int32(ndata))
+
+    greg(cuda.In(data.astype('float32')), cuda.Out(out), cuda.In(frequencies.astype('float32')), np.float32(0.5),
+            block=(blockSize,1,1), grid=(gridSize,1,1))
+
+    return out
+
+def findPeaks(data, frequencies):
+    '''
+    data is a ndata*frequency array
+    '''
+    data = data.T
+    nfrequencies, ndata = data.shape
+    out = np.empty(ndata, 'float32')
+
+    blockSize = 512
+    gridSize = (ndata + blockSize - 1) / blockSize
+
+    cuda.memcpy_htod(mod.get_global('numFreqs')[0], np.int32(nfrequencies))
+    cuda.memcpy_htod(mod.get_global('numPoints')[0], np.int32(ndata))
+
+    fp = mod.get_function('findPeaks')
+
+    fp(cuda.In(data.astype('float32')), cuda.Out(out), cuda.In(frequencies.astype('float32')),
             block=(blockSize,1,1), grid=(gridSize,1,1))
 
     return out
